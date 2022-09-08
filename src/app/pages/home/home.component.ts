b2777de5301bffa35bba1212dfe43cd51feb7eec
martinagorriaran <martina.gorriaran@gmail.com>
2022-09-08 08:48:15 -0300

    menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from 'src/app/interfaces/menu';
 import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
@@ -8,12 +9,17 @@ import { MenuService } from 'src/app/services/menu.service';
 })
 export class HomeComponent implements OnInit {
 
+  menus:Menu[] = []
+
 
   constructor(private servicioMenus: MenuService) {
 
   }
 
   ngOnInit(): void {
+    this.servicioMenus.getMenu().subscribe((menu)=>{
+      this.menus = menu
+    })
   }
 
 }
